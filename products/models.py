9f8ec2470a969ef2dfdb4334b184e00b97bf7d06
Sophia_Vikentiva <vikentevasof@gmail.com>
2026-05-17 22:37:37 +0300

    Added ORM pages and category filtering

--- products/models.py
+++ products/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='products'
+    )
+
     title = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
--- products/views.py
+++ products/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 
 def product_list(request):
+    category_id = request.GET.get("category")
+
     products = Product.objects.all()
+    categories = Category.objects.all()
+
+    if category_id:
+        products = products.filter(category_id=category_id)
 
     context = {
-        "products": products
+        "products": products,
+        "categories": categories
     }
 
     return render(request, "product_list.html", context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+
+    return render(
+        request,
+        "product_detail.html",
+        {"product": product}
+    )
